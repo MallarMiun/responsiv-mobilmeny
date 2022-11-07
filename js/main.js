e3c2000a718832e31eff46439041d6828ae0aa5f
Malin Larsson <malin.larsson@miun.se>
2022-11-07 13:18:55 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+"use strict"
+
+// Hämta in meny-knapparna
+let openBtn = document.getElementById("open-menu");
+let closeBtn = document.getElementById("close-menu");
+
+//eventlyssnare
+openBtn.addEventListener('click', toggleMenu);
+closeBtn.addEventListener('click', toggleMenu);
+
+//Toggla fram navigeringsmenyn
+function toggleMenu() {
+    let navMenuEl = document.getElementById("nav-menu");
+
+    //hämtar in css för menyn
+    let style = window.getComputedStyle(navMenuEl);
+
+    //koll om navigering är synlig eller ej, ändrar display block/none
+    if(style.display === "none") {
+        navMenuEl.style.display = "block";
+    } else {
+        navMenuEl.style.display = "none";
+    }
+}
